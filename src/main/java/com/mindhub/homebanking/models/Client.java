ede6f28514419f0e70853286c5bdba35ef258ec3
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-12-11 08:42:35 -0500

    Add setters for Client

--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -51,6 +51,22 @@ public class Client {
         return accounts;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
     @Override
     public String toString() {
         return "Client{" +
